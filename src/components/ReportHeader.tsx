9d76e8c9347592a5720d8d875db5e0bb44cb85db
Saally Campos <88331924+saaaaaally@users.noreply.github.com>
2021-12-03 13:54:20 -0600

    Add Ids for Tech Support context menu

--- src/components/ReportHeader.tsx
+++ src/components/ReportHeader.tsx
@@ -1,17 +1,38 @@
-import { useState, useEffect } from 'react';
-import { Button } from '@itwin/itwinui-react';
+import { useState, useEffect, useCallback } from 'react';
+import { Button, IconButton, Tooltip } from '@itwin/itwinui-react';
 import {
   SvgExport,
   SvgClose,
   SvgStatusSuccess,
   SvgStatusWarning,
   SvgStatusError,
-  SvgInfoCircular
+  SvgInfoCircular,
+  SvgCopy
 } from '@itwin/itwinui-icons-react/cjs/icons';
 import { ReportData } from './typings';
 import './ReportHeader.scss';
 
-export const ReportHeader = ({ data }: { data?: ReportData }) => {
+export type ReportHeaderProps = {
+  data: ReportData;
+  contextName?: string; //Todo: How to pass in these props
+  jobDefID?: string;
+  jobRunID?: string;
+  modelName?: string;
+  organizationName?: string;
+  userEmail?: string;
+  /// If defined and `onCreateServiceRequestClick` is not defined, this url will be opened in a new tab when creating a new service request
+  serviceReportUrl?: string;
+  /// If defined, clicking on the button to create a service request will call this function.
+  onCreateServiceRequestClick?: () => void;
+};
+
+export const ReportHeader = (props: ReportHeaderProps) => {
+  const {
+    data,
+    serviceReportUrl,
+    onCreateServiceRequestClick,
+  } = props;
+
   const [showSupport, setShowSupport] = useState(false);
   const [errorCount, setErrorCount] = useState(0);
   const [warningCount, setWarningCount] = useState(0);
@@ -61,6 +82,47 @@ export const ReportHeader = ({ data }: { data?: ReportData }) => {
       );
     }, [data])
 
+  const activityID = data?.context?.activityid ?? 'No Activity ID';
+  const briefcaseID = data?.context?.briefcaseid ?? 'No Briefcase ID';
+  const contextId = data?.context?.contextid ?? 'No Context ID';
+  const contextName = props.contextName ?? 'No Context Name';
+  const jobDefID = props.jobDefID ?? 'No Job Definition ID';
+  const jobID = data?.context?.jobid ?? 'No Job ID';
+  const jobRunID = props.jobRunID ?? 'No Job Run ID';
+  const modelID = data?.context?.imodelid ?? 'No iModel ID';
+  const modelName = props.modelName ?? 'No Model Name';
+  const organizationName = props.organizationName ?? 'No Organization Name';
+  const userEmail = props.userEmail ?? 'No User Email';
+
+  const handleCopyTextClicked = ()=>{
+    // DO NOT LOCALIZE!!! This is used for pasting into the service request form in English.
+    const debugString = `
+      Activity ID: ${activityID}
+      Briefcase ID: ${briefcaseID}
+      Context ID: ${contextId}
+      Ctx. Name: ${contextName}
+      Job Def. ID: ${jobDefID}
+      Job ID: ${jobID}
+      Job Run ID: ${jobRunID}
+      iModel ID: ${modelID}
+      iModel Name: ${modelName}
+      Org. Name: ${organizationName}
+      User Email: ${userEmail}`;
+
+     navigator.clipboard.writeText(debugString);
+    };
+
+    const onCreateSrClick = useCallback(() => {
+      if (onCreateServiceRequestClick) {
+        onCreateServiceRequestClick();
+        return;
+      }
+  
+      if (serviceReportUrl) {
+        window.open(serviceReportUrl, '_blank');
+      }
+    }, [serviceReportUrl, onCreateServiceRequestClick]);
+
 
   return (
     <>
@@ -74,10 +136,69 @@ export const ReportHeader = ({ data }: { data?: ReportData }) => {
             <Button startIcon={<SvgClose />} >Close</Button>
           </div>
 
-          <div className='isr-header-support' onClick={() => (setShowSupport(!showSupport))}>{'IDs for Tech Support'}</div>
-          {showSupport && (
-            <div className='isr-support-contextMenu'>Context</div>
+          <div className='isr-header-supportWrapper'>
+            <div className='isr-support-button' onClick={() => (setShowSupport(!showSupport))}>
+              {'IDs for Tech Support'}
+            </div>
+            {showSupport && (
+            <div className='isr-support-debugIDWrapper'>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Activity ID:</div>
+                <div className='isr-support-debugID-id'>{activityID}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Briefcase ID:</div>
+                <div className='isr-support-debugID-id'>{briefcaseID}</div>
+              </div>
+              <div className='isr-support-debugID'>
+              <div className='isr-support-debugID-title'>Context ID:</div>
+                <div className='isr-support-debugID-id'>{contextId}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Ctx. Name:</div>
+                <div className='isr-support-debugID-id'>{contextName}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Job Def. ID:</div>
+                <div className='isr-support-debugID-id'>{jobDefID}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Job ID:</div>
+                <div className='isr-support-debugID-id'>{jobID}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Job Run ID:</div>
+                <div className='isr-support-debugID-id'>{jobRunID}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Model ID:</div>
+                <div className='isr-support-debugID-id'>{modelID}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Model Name:</div>
+                <div className='isr-support-debugID-id'>{modelName}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>Org. Name:</div>
+                <div className='isr-support-debugID-id'>{organizationName}</div>
+              </div>
+              <div className='isr-support-debugID'>
+                <div className='isr-support-debugID-title'>User Email:</div>
+                <div className='isr-support-debugID-id'>{userEmail}</div>
+              </div>
+              <div className='isr-support-debugID-buttons'>
+                <Tooltip content={'Copy to clipboard and paste in service request'} placement='bottom'>
+                  <IconButton onClick={handleCopyTextClicked}>
+                    <SvgCopy />
+                  </IconButton>
+                </Tooltip>
+                <Button styleType='cta' onClick={onCreateSrClick}>
+                  {'Create Service Request'}
+                </Button></div>
+              </div>
           )}
+          </div>
+
           <div className='isr-header-issues'>
 
           {issuesCount > 0 ? (
