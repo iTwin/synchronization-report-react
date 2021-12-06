99fbf63d676b071a25d1d48c7890c441bf12ff1b
Saally Campos <88331924+saaaaaally@users.noreply.github.com>
2021-12-06 15:18:58 -0600

    Add iTwinUI-css variables

--- src/components/ReportHeader.tsx
+++ src/components/ReportHeader.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect, useCallback } from 'react';
 import { Button, IconButton, Tooltip } from '@itwin/itwinui-react';
 import {
@@ -7,7 +8,7 @@ import {
   SvgStatusWarning,
   SvgStatusError,
   SvgInfoCircular,
-  SvgCopy
+  SvgCopy,
 } from '@itwin/itwinui-icons-react/cjs/icons';
 import { ReportData } from './typings';
 import './ReportHeader.scss';
@@ -27,24 +28,20 @@ export type ReportHeaderProps = {
 };
 
 export const ReportHeader = (props: ReportHeaderProps) => {
-  const {
-    data,
-    serviceReportUrl,
-    onCreateServiceRequestClick,
-  } = props;
+  const { data, serviceReportUrl, onCreateServiceRequestClick } = props;
 
   const [showSupport, setShowSupport] = useState(false);
   const [errorCount, setErrorCount] = useState(0);
   const [warningCount, setWarningCount] = useState(0);
   const [infoCount, setInfoCount] = useState(0);
   const [issuesCount, setIssuesCount] = useState(0);
-  useEffect(()=>{
+  useEffect(() => {
     let error = 0;
     let warning = 0;
     let info = 0;
-    data?.filerecords?.forEach((file)=>{
-      file.auditrecords?.forEach((record)=>{
-        switch (record.auditinfo?.level){
+    data?.filerecords?.forEach((file) => {
+      file.auditrecords?.forEach((record) => {
+        switch (record.auditinfo?.level) {
           case 'Error':
             error++;
             break;
@@ -55,32 +52,25 @@ export const ReportHeader = (props: ReportHeaderProps) => {
             info++;
             break;
         }
-      })
-    })
+      });
+    });
     setErrorCount(error);
     setWarningCount(warning);
     setInfoCount(info);
     setIssuesCount(error + warning + info);
-  }, [data])
+  }, [data]);
 
   const [date, setDate] = useState<string>('date');
-  useEffect(()=>{
-      const options: Intl.DateTimeFormatOptions = {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-      };
-      setDate(
-        data?.context?.timestamp
-          ? new Date(data.context.timestamp).toLocaleDateString(
-              undefined,
-              options
-            )
-          : ''
-      );
-    }, [data])
+  useEffect(() => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+    };
+    setDate(data?.context?.timestamp ? new Date(data.context.timestamp).toLocaleDateString(undefined, options) : '');
+  }, [data]);
 
   const activityID = data?.context?.activityid ?? 'No Activity ID';
   const briefcaseID = data?.context?.briefcaseid ?? 'No Briefcase ID';
@@ -94,7 +84,7 @@ export const ReportHeader = (props: ReportHeaderProps) => {
   const organizationName = props.organizationName ?? 'No Organization Name';
   const userEmail = props.userEmail ?? 'No User Email';
 
-  const handleCopyTextClicked = ()=>{
+  const handleCopyTextClicked = () => {
     // DO NOT LOCALIZE!!! This is used for pasting into the service request form in English.
     const debugString = `
       Activity ID: ${activityID}
@@ -109,38 +99,39 @@ export const ReportHeader = (props: ReportHeaderProps) => {
       Org. Name: ${organizationName}
       User Email: ${userEmail}`;
 
-     navigator.clipboard.writeText(debugString);
-    };
+    navigator.clipboard.writeText(debugString);
+  };
 
-    const onCreateSrClick = useCallback(() => {
-      if (onCreateServiceRequestClick) {
-        onCreateServiceRequestClick();
-        return;
-      }
-  
-      if (serviceReportUrl) {
-        window.open(serviceReportUrl, '_blank');
-      }
-    }, [serviceReportUrl, onCreateServiceRequestClick]);
+  const onCreateSrClick = useCallback(() => {
+    if (onCreateServiceRequestClick) {
+      onCreateServiceRequestClick();
+      return;
+    }
 
+    if (serviceReportUrl) {
+      window.open(serviceReportUrl, '_blank');
+    }
+  }, [serviceReportUrl, onCreateServiceRequestClick]);
 
   return (
     <>
       <div className='isr-report-header'>
-          <div className='isr-header-title-wrapper'>
-            <div className='isr-header-title'>{'Synchronization Report | ' + data?.sourceFilesInfo?.fileName ?? 'unknown'}</div>
-            <div className='isr-header-titleDate'>{'Run Completed: ' + date}</div>
+        <div className='isr-header-title-wrapper'>
+          <div className='isr-header-title'>
+            {'Synchronization Report | ' + data?.sourceFilesInfo?.fileName ?? 'unknown'}
           </div>
-          <div className='isr-header-close'>
-            <Button startIcon={<SvgExport />} >Export</Button>
-            <Button startIcon={<SvgClose />} >Close</Button>
-          </div>
+          <div className='isr-header-titleDate'>{'Run Completed: ' + date}</div>
+        </div>
+        <div className='isr-header-close'>
+          <Button startIcon={<SvgExport />}>Export</Button>
+          <Button startIcon={<SvgClose />}>Close</Button>
+        </div>
 
-          <div className='isr-header-supportWrapper'>
-            <div className='isr-support-button' onClick={() => (setShowSupport(!showSupport))}>
-              {'IDs for Tech Support'}
-            </div>
-            {showSupport && (
+        <div className='isr-header-supportWrapper'>
+          <div className='isr-support-button' onClick={() => setShowSupport(!showSupport)}>
+            {'IDs for Tech Support'}
+          </div>
+          {showSupport && (
             <div className='isr-support-debugIDWrapper'>
               <div className='isr-support-debugID'>
                 <div className='isr-support-debugID-title'>Activity ID:</div>
@@ -151,7 +142,7 @@ export const ReportHeader = (props: ReportHeaderProps) => {
                 <div className='isr-support-debugID-id'>{briefcaseID}</div>
               </div>
               <div className='isr-support-debugID'>
-              <div className='isr-support-debugID-title'>Context ID:</div>
+                <div className='isr-support-debugID-title'>Context ID:</div>
                 <div className='isr-support-debugID-id'>{contextId}</div>
               </div>
               <div className='isr-support-debugID'>
@@ -194,44 +185,43 @@ export const ReportHeader = (props: ReportHeaderProps) => {
                 </Tooltip>
                 <Button styleType='cta' onClick={onCreateSrClick}>
                   {'Create Service Request'}
-                </Button></div>
+                </Button>
               </div>
+            </div>
           )}
-          </div>
+        </div>
 
-          <div className='isr-header-issues'>
-
+        <div className='isr-header-issues'>
           {issuesCount > 0 ? (
             <>
               <div>{issuesCount + ' Synchronization Issues |'}</div>
               {errorCount > 0 && (
                 <>
-                <SvgStatusError className='isr-status-icon isr-negative'/>
-                <div>{'Errors: ' + errorCount + ' |'}</div>
+                  <SvgStatusError className='isr-status-icon isr-negative' />
+                  <div>{'Errors: ' + errorCount + ' |'}</div>
                 </>
               )}
               {warningCount > 0 && (
                 <>
-                <SvgStatusWarning className='isr-status-icon isr-warning'/>
-                <div>{'Warnings: ' + warningCount + ' |'}</div>
+                  <SvgStatusWarning className='isr-status-icon isr-warning' />
+                  <div>{'Warnings: ' + warningCount + ' |'}</div>
                 </>
               )}
               {infoCount > 0 && (
                 <>
-                <SvgInfoCircular className='isr-status-icon isr-informational'/>
-                <div>{'Other Issues: ' + infoCount }</div>
+                  <SvgInfoCircular className='isr-status-icon isr-informational' />
+                  <div>{'Other Issues: ' + infoCount}</div>
                 </>
               )}
-              </>
-          ): (
-          <>
-          <SvgStatusSuccess className='isr-status-icon isr-positive'/>
-          <div>{'No Issues Found' }</div>
-          </>
+            </>
+          ) : (
+            <>
+              <SvgStatusSuccess className='isr-status-icon isr-positive' />
+              <div>{'No Issues Found'}</div>
+            </>
           )}
-          </div>
         </div>
+      </div>
     </>
-
   );
 };
