56ec9571f82b03bfee6e00f2488f468821014878
Mayank <mayank99@users.noreply.github.com>
2021-12-14 17:18:40 -0500

    Extract `ReportTabs` into separate component

---
+++ src/components/ReportTabs.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import classnames from 'classnames';
+import { HorizontalTabs, Tab } from '@itwin/itwinui-react';
+import { ReportContext } from './Report';
+import './ReportTabs.scss';
+
+const defaultDisplayStrings = {
+  files: 'Files',
+  details: 'Details',
+} as const;
+
+export const ReportTabs = ({
+  displayStrings: userDisplayStrings,
+  filesTable,
+  detailsTable,
+  className,
+  ...rest
+}: {
+  displayStrings?: typeof defaultDisplayStrings;
+  filesTable: React.ReactNode;
+  detailsTable: React.ReactNode;
+  className?: string;
+}) => {
+  const context = React.useContext(ReportContext);
+
+  if (!context) {
+    throw 'ReportTabs must be used inside the Report component';
+  }
+
+  const { currentTab, setCurrentTab } = context;
+
+  const displayStrings = { ...defaultDisplayStrings, ...userDisplayStrings };
+
+  return (
+    <HorizontalTabs
+      activeIndex={currentTab === 'files' ? 0 : 1}
+      onTabSelected={(index) => setCurrentTab(index === 0 ? 'files' : 'details')}
+      labels={[
+        <Tab key='files' label={displayStrings['files']} />,
+        <Tab key='details' label={displayStrings['details']} />,
+      ]}
+      type='borderless'
+      contentClassName='isr-report-tabs-content'
+      wrapperClassName={classnames('isr-report-tabs-wrapper', className)}
+      {...rest}
+    >
+      {currentTab === 'files' ? filesTable : detailsTable}
+    </HorizontalTabs>
+  );
+};
