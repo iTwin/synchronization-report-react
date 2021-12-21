5651ac72029ed1dbb1f56a084cfda4b7c9f8bfea
Mayank <9084735+mayank99@users.noreply.github.com>
2021-12-21 07:32:11 -0500

    Split up ReportTabs into `ReportTablist` and `ReportTabpanel` (#14)
    The split improves the API and allows for extra flexibility for things like customization and layout, e.g. the ability to put a searchbar and extra buttons in the same line as the tablist.

---
+++ src/components/ReportTabpanel.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import classnames from 'classnames';
+import { FilesTable } from './FilesTable';
+import { DetailsTable } from './DetailsTable';
+import { ReportContext } from './Report';
+import './ReportTabpanel.scss';
+
+/**
+ * `ReportTabpanel` shows the `FilesTable` or `DetailsTable` depending on the active tab.
+ */
+export const ReportTabpanel = ({
+  currentTab,
+  children,
+  className,
+  ...rest
+}: {
+  /**
+   * Current tab. Will be inferred from context if not specified.
+   */
+  currentTab?: 'files' | 'details';
+  /**
+   * Two `children` can be specified to override the tables.
+   * @default
+   * [<FilesTable />, <DetailsTable />]
+   */
+  children?: [React.ReactNode, React.ReactNode];
+  className?: string;
+  id?: string;
+}) => {
+  const context = React.useContext(ReportContext);
+  if (!currentTab) {
+    if (!context) {
+      throw new Error('currentTab must be specified or this component must be used inside Report');
+    }
+    currentTab = context.currentTab;
+  }
+
+  const filesTable = children?.[0] ?? <FilesTable />;
+  const detailsTable = children?.[1] ?? <DetailsTable />;
+
+  return (
+    <div className={classnames('isr-report-tabpanel', className)} role='tabpanel' {...rest}>
+      {currentTab === 'files' ? filesTable : detailsTable}
+    </div>
+  );
+};
