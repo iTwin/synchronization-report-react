cfe07c7f78934fbe469a602ab4fdcc63e0b61654
Will McRae <Will.McRae@bentley.com>
2022-01-10 14:24:19 -0600

    en: add copyright headers to all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+ * See LICENSE.md in the project root for license terms and full copyright notice.
+ *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
 import { IconButton, Tooltip, useTheme } from '@itwin/itwinui-react';
 import { useMediaQuery } from 'beautiful-react-hooks';
