19bfe647213d4b28511f096a9a44750be4d49126
Guillar1 <Guillar1@users.noreply.github.com>
2022-10-20 15:27:48 +0300

    Update ClampWithTooltip.tsx

--- src/components/utils/ClampWithTooltip.tsx
+++ src/components/utils/ClampWithTooltip.tsx
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 import * as React from 'react';
 import { Tooltip } from '@itwin/itwinui-react';
-import SvgInfoCircular from '@itwin/itwinui-icons-react/esm/icons/InfoCircular';
 import './ClampWithTooltip.scss';
 import classnames from 'classnames';
 
@@ -16,28 +15,11 @@ export const ClampWithTooltip = ({
   children: React.ReactNode;
   className?: string;
 }) => {
-  const [isOverflowing, setIsOverflowing] = React.useState(false);
-
   return (
-    <>
-      <span
-        className={classnames('isr-line-clamp', className)}
-        ref={(el) => {
-          if (el) {
-            setIsOverflowing(el.scrollHeight > el.offsetHeight);
-          }
-        }}
-        {...rest}
-      >
+    <Tooltip content={children} appendTo={() => document.body}>
+      <span className={classnames('isr-line-clamp', className)} {...rest}>
         {children}
       </span>
-      {isOverflowing && (
-        <Tooltip content={children} appendTo={() => document.body}>
-          <span aria-hidden className='isr-tooltip-icon'>
-            <SvgInfoCircular />
-          </span>
-        </Tooltip>
-      )}
-    </>
+    </Tooltip>
   );
 };
