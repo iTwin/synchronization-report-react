f0a2a9bef776b347152f01e5b4530b10477fa793
Simi Hartstein <simihartstein@users.noreply.github.com>
2023-09-12 18:07:24 -0400

    Updated links to help articles

--- src/components/help-articles.ts
+++ src/components/help-articles.ts
@@ -2,26 +2,28 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-const helpArticleBaseUrl =
-  'https://communities.bentley.com/products/digital-twin-cloud-services/itwin-services/w/synchronization-wiki/68150/synchronization-report-knowledge-base';
+const SyncWikiBaseUrl =
+  'https://communities.bentley.com/products/digital-twin-cloud-services/itwin-services/w/synchronization-wiki';
 
-// list of errors that contain a help article
-const helpArticleErrors = [
-  'Dgn_0000',
-  'Dgn_0066',
-  'Dgn_0199',
-  'Dgn_0205',
-  'Dgn_0214',
-  'Dgn_0215',
-  'Revit_0002',
-  'Revit_0025',
-];
+// map of errors with help articles along with their URL
+const helpArticles: { [issueId: string]: string } = {
+  Dgn_0000: `${SyncWikiBaseUrl}/68291/missing-data#Dgn_0000`,
+  Dgn_0066: `${SyncWikiBaseUrl}/68292/inconsistent-data#Dgn_0066`,
+  Dgn_0199: `${SyncWikiBaseUrl}/68521/unexpected-data#Dwg_0199`,
+  Dgn_0205: `${SyncWikiBaseUrl}/68077/itwin-extents#OutlierElements`,
+  Dgn_0214: `${SyncWikiBaseUrl}/68291/missing-data#Dgn_0214`,
+  // "Dgn_0215": ``,
+  Dwg_0203: `"${SyncWikiBaseUrl}/68522/data-access#Dwg_0203`,
+  Dwg_0199: `${SyncWikiBaseUrl}/68521/unexpected-data#Dwg_0199`,
+  Revit_0002: `${SyncWikiBaseUrl}/68291/missing-data#Revit0002`,
+  Revit_0025: `${SyncWikiBaseUrl}/68291/missing-data#Revit0025`,
+};
 
-export function hasHelpArticle(errorId: string | undefined): boolean {
-  if (!errorId) return false;
-  return helpArticleErrors.includes(errorId);
+export function hasHelpArticle(issueId: string | undefined): boolean {
+  if (!issueId) return false;
+  return helpArticles[issueId] !== undefined;
 }
 
-export function getHelpArticleUrl(errorId: string): string {
-  return `${helpArticleBaseUrl}#${errorId}`;
+export function getHelpArticleUrl(issueId: string): string {
+  return helpArticles[issueId];
 }
