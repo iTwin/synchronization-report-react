5873b833e585ce4d9965437e21da96c3972eb63a
Simi Hartstein <simihartstein@users.noreply.github.com>
2023-09-19 09:55:33 -0400

    Fixed url for Dgn_0199

--- src/components/help-articles.ts
+++ src/components/help-articles.ts
@@ -9,7 +9,7 @@ const SyncWikiBaseUrl =
 const helpArticles: { [issueId: string]: string } = {
   Dgn_0000: `${SyncWikiBaseUrl}/68291/missing-data#Dgn_0000`,
   Dgn_0066: `${SyncWikiBaseUrl}/68292/inconsistent-data#Dgn_0066`,
-  Dgn_0199: `${SyncWikiBaseUrl}/68521/unexpected-data#Dwg_0199`,
+  Dgn_0199: `${SyncWikiBaseUrl}/68292/inconsistent-data#dgn_0199`,
   Dgn_0205: `${SyncWikiBaseUrl}/68077/itwin-extents#OutlierElements`,
   Dgn_0214: `${SyncWikiBaseUrl}/68291/missing-data#Dgn_0214`,
   // "Dgn_0215": ``,
